23797ce040a49b770ada7b560e3bf48e3df64094
Kittu <bharath72kumar@gmail.com>
2018-05-29 15:40:38 +0530

    update with largedata

--- kmeans.py
+++ kmeans.py
@@ -1,4 +1,10 @@
 #K-Means Unsuper
+from dataset import data
+from sklearn import linear_model,datasets
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error,r2_score
+import numpy as np
+train_x,train_y,test_x,test_y = data()
 from sklearn.cluster import KMeans
 model = KMeans(n_clusters=3,random_state=0)
 model.fit(train_x,train_y)
