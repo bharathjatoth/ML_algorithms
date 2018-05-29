dd0da30715317c7ef7d87760a3ac5330e451eea7
Kittu <bharath72kumar@gmail.com>
2018-05-29 14:36:49 +0530

    update

---
+++ logistic_regression.py
@@ -0,0 +1,14 @@
+from sklearn import linear_model,datasets
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error,r2_score
+import numpy as np
+
+#logistic Regression model which predicts the outcome as 0/1, True/False
+#to improve this we can use regularisation, non-linear model, feature scaling
+model = linear_model.LogisticRegression(C=100,penalty='l2')
+model.fit(train_x,train_y)
+pred = model.predict(test_x)
+plt.scatter(train_x,train_y)
+plt.plot(test_x,pred)
+plt.show()
+print(pred)
